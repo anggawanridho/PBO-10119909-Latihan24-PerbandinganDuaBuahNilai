3d93a0c0f44e4294163961654c4e10be861016d7
anggawanridho <55708748+anggawanridho@users.noreply.github.com>
2020-10-28 13:03:15 +0700

    Add files via upload

---
+++ src/perbandingan_dua_buah_nilai/PBO10119909Latihan24.java
@@ -0,0 +1,39 @@
+package perbandingan_dua_buah_nilai;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhammad Anggawan Ridho Islami
+ * KELAS    : IF 10K
+ * NIM      : 10119909
+ */
+
+public class PBO10119909Latihan24 {
+
+    public static void main(String[] args) {
+        int nilai1, nilai2, hasil;
+        String pilihan;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=====Program Perbandingan Nilai=====");
+        do {
+            System.out.print("Masukkan nilai Pertama : ");
+            nilai1 = scanner.nextInt();
+            System.out.print("Masukkan nilai Kedua : ");
+            nilai2 = scanner.nextInt();
+            if (nilai1 < nilai2){
+                System.out.printf("Hasil : %1$d Lebih Kecil dari %2$d%n", nilai1, nilai2);
+            }
+            if (nilai1 > nilai2){
+                System.out.printf("Hasil : %1$d Lebih Besar dari %2$d%n", nilai1, nilai2);
+            }
+            if (nilai1 == nilai2){
+                System.out.printf("Hasil : %1$d Sama Dengan %2$d%n", nilai1, nilai2);
+            }
+            System.out.print("Ulangi Aktifitas? Ya/Tidak : ");
+            pilihan = scanner.next();
+            System.out.println();
+        } while (pilihan.equals("ya") || pilihan.equals("Ya"));
+    }
+}
